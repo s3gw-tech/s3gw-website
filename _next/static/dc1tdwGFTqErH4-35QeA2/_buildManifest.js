f88268ccf28fb490559ae3a178959f1e4a338f19
en3sis <en3sis@gmail.com>
2021-11-10 13:18:15 +0000

    Deploying to gh-pages from @ SUSE/project-template@32488b3fbdf197f343addcca8d73b3fabbf993e7 🚀

---
+++ _next/static/dc1tdwGFTqErH4-35QeA2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fde11a7e3c31308b.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
