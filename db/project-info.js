d6c0e2aa7113c44b08ab6de9289af573945dbd33
Alexandra Settle <asettle@suse.com>
2022-01-10 13:31:38 +0000

    website: updating content info

--- db/project-info.js
+++ db/project-info.js
@@ -3,66 +3,62 @@ import AWS_LOGO from '@/assets/sponsors/aws.svg'
 import AZURE_LOOG from '@/assets/sponsors/azure.svg'
 
 export const PROJECT_CONFIGURATION = {
-  projectTitle: 'Project Name',
-  projectSubtitle: 'Short project description, it will be used on the header',
+  projectTitle: 'Aquarium',
+  projectSubtitle: 'An easy-to-use, rock solid storage appliance built on Ceph',
   projectNavigation: {
     links: [
       {
         title: 'Documentation',
-        href: 'https://www.google.com'
+        href: 'https://github.com/aquarist-labs/aquarium/blob/main/doc/from-zero-to-hero.md'
       },
       {
         title: 'Blog',
-        href: 'https://www.google.com'
+        href: 'https://www.suse.com/c/inside-the-aquarium-investigating-a-storage-appliance-built-on-ceph/'
       }
     ],
     social: [
       {
-        url: 'https://www.google.com',
+        url: 'https://github.com/aquarist-labs/aquarium',
         icon: GithubSVG
       },
-      {
-        url: 'https://www.google.com',
-        icon: TwitterSVG
-      }
     ]
   },
   projectVideo: {
-    isEnable: false,
-    youtubeUrl: 'https://www.youtube.com/watch?v=6pYrg92TOWo'
+    isEnable: true,
+    youtubeUrl: 'https://www.youtube.com/watch?v=C_2wvFDgh9g&t=631s'
   },
   projectCta: {
-    title: 'About Project Name',
-    description: 'In-memory image reduces upgrade overhead (minimized system upgrade impact) easy installer wizard for persistent disk and other tooling. minimized system upgrade impact) easy installer wizard for persistent disk and other tooling.'
+    title: 'About Aquarium',
+    description: 'Aquarium is '
   },
   // ℹ️ No more than 3 items for a better structure.
   projectBulletPoints: [
-    'Building, running, pushing, and pulling containers',
-    'Running your choice of Kubernetes versions',
-    'Local application development in Kubernetes'
+    'A storage appliance build in an opinionated fashion',
+    'Easy to manage deployment and day 1 operations',
+    'Simple management UI with an intuitive UI/UX experience'
   ],
-  projectRepoURL: 'URL',
-  projectDocumentationURL: 'URL',
+  projectRepoURL: 'https://github.com/aquarist-labs/aquarium',
+  projectDocumentationURL: 'https://github.com/aquarist-labs/aquarium/blob/main/doc/from-zero-to-hero.md',
   // ℹ️ No more than 3 items for a better structure.
   projectWhy: [
     {
       icon: <CloudSVG />,
-      title: `Kubernetes Made Simple`,
-      description: `Getting started with Kubernetes on your desktop can be a project. Especially if you want to match the version of Kubernetes you run locally to the one you run in production. Rancher Desktop makes it as easy as setting a preference.`,
+      title: `Ceph Made Simple`,
+      description: `Ceph is too complex to use for the average user. This complexity stems from Ceph’s flexibility that supports a huge matrix of use cases. Aquarium simplifies the installation, deployment, and day 1 operations.`,
     },
     {
       icon: <TorchSVG />,
       title: `Built On Proven Projects`,
-      description: `Getting started with Kubernetes on your desktop can be a project. Especially if you want to match the version of Kubernetes you run locally to the one you run in production. Rancher Desktop makes it as easy as setting a preference.`,
+      description: `Ceph is the industry standard for providing object, file, and block storage. Aquarium leverages Ceph and transforms support for object and file storage.`,
     },
     {
       icon: <ContainerSVG />,
-      title: `Coupled Container Management`,
-      description: `Getting started with Kubernetes on your desktop can be a project. Especially if you want to match the version of Kubernetes you run locally to the one you run in production. Rancher Desktop makes it as easy as setting a preference.`,
+      title: `Container Management`,
+      description: `Leveraging the Ceph Orchestrator (cephadm), Aquarium deploys and manages a Ceph cluster by connection to hosts from the manager daemon via SSH to add, remove, or update Ceph daemon containers. It does not rely on external configuration or orchestration tools like Ansible, Rook, or Salt.`,
     }
   ],
   projectHow: {
-    description: 'Rancher Desktop is an electron based application that wraps other tools while itself providing the user experience to create a simple experience. On MacOS Rancher Desktop leverages a virtual machine to run containerd and Kubernetes. Windows Subsystem for Linux v2 is leveraged for Windows systems. All you need to do is download and run the application.'
+    description: 'Aquarium is a standalone storage appliance that can .'
   },
   // ℹ️ No more than 3 items for a better structure.
   projectGetStarted: [
@@ -89,70 +85,50 @@ export const PROJECT_CONFIGURATION = {
     elements:
       [
         {
-          title: 'What is SUSE eLearning?',
-          description: 'SUSE eLearning is your on-demand library of all SUSE technical training. A yearly subscription to SUSE eLearning gives you access to limitless learning to the entire library of SUSE training — anywhere, anytime — with immediate access to updated courseware.',
+          title: 'What is a storage appliance?',
+          description: 'A storage appliance is something that can provide or manage data without an application context to add simplification to the day 1 installer experience. This is often displayed in the form of a wizard-driven installation process, but it can also mean a fixed hardware configuration.',
         },
         {
-          title: 'What is SUSE eLearning?',
-          description: 'SUSE eLearning is your on-demand library of all SUSE technical training. A yearly subscription to SUSE eLearning gives you access to limitless learning to the entire library of SUSE training — anywhere, anytime — with immediate access to updated courseware.',
+          title: 'How is this project related to Ceph?',
+          description: 'Being Ceph developers and enthusiasts, we’re building Aquarium on top of Ceph. Providing an appliance style deployment and a service-based GUI, but it’s still Ceph underneath.',
         },
         {
-          title: 'What is SUSE eLearning?',
-          description: 'SUSE eLearning is your on-demand library of all SUSE technical training. A yearly subscription to SUSE eLearning gives you access to limitless learning to the entire library of SUSE training — anywhere, anytime — with immediate access to updated courseware.',
+          title: 'Is this an open source project?',
+          description: 'Absolutely! Everything we’re doing is in the open: development, discussions, decision making, even our meetings.',
         },
         {
-          title: 'What is SUSE eLearning?',
-          description: 'SUSE eLearning is your on-demand library of all SUSE technical training. A yearly subscription to SUSE eLearning gives you access to limitless learning to the entire library of SUSE training — anywhere, anytime — with immediate access to updated courseware.',
+          title: 'How do I get involved?',
+          description: 'Have a look at our issue list in the GitHub repository and please join our Slack channel. We would love to hear from you.',
+        },
+
+        {
+          title: 'Does Aquarium support the same services and features as Ceph?',
+          description: 'We deliberately do not expose all features in order to simplify deployment and management for what we think are common or useful deployment scenarios.',
         },
       ]
   },
   projectSponsors: {
-    isEnable: true,
+    isEnable: false,
     elements: [
       {
         sponsor: 'aws',
         href: 'http://www.google.com',
         logo: AWS_LOGO
       },
-      {
-        sponsor: 'azure',
-        href: 'http://www.google.com',
-        logo: AZURE_LOOG,
-      },
-      {
-        sponsor: 'aws',
-        href: 'http://www.google.com',
-        logo: AWS_LOGO
-      },
-      {
-        sponsor: 'azure',
-        href: 'http://www.google.com',
-        logo: AZURE_LOOG,
-      },
-      {
-        sponsor: 'aws',
-        href: 'http://www.google.com',
-        logo: AWS_LOGO
-      },
-      {
-        sponsor: 'azure',
-        href: 'http://www.google.com',
-        logo: AZURE_LOOG,
-      },
     ]
   },
   projectFooterLinks: [
     {
-      title: 'Carrer',
-      href: 'http://www.google.com',
+      title: 'Career',
+      href: 'https://jobs.suse.com/',
     },
     {
       title: 'Legal',
-      href: 'http://www.google.com',
+      href: 'https://www.suse.com/company/legal/',
     },
     {
       title: 'Feedback',
-      href: 'http://www.google.com',
+      href: 'https://github.com/aquarist-labs/aquarium/issues',
     },
   ]
 }
