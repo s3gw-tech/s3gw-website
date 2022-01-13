87fbdb812cf4ee7577d6a5221572280ed43ebf2b
en3sis <940170+en3sis@users.noreply.github.com>
2022-01-13 14:59:32 +0000

    Deploying to gh-pages from @ aquarist-labs/project-template@aa4b3f8918121dfbd4b3fafec0613b03cce39031 🚀

--- _next/static/pCrTj7CNYydHfBq7zgsXy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/687-5df33ef8761d4840.js","static/chunks/pages/index-c5d614da31e23416.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
