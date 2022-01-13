aa4b3f8918121dfbd4b3fafec0613b03cce39031
en3sis <940170+en3sis@users.noreply.github.com>
2022-01-13 15:58:40 +0100

    Fixes diagram
    Signed-off-by: en3sis <940170+en3sis@users.noreply.github.com>

--- components/_layout/Header.tsx
+++ components/_layout/Header.tsx
@@ -13,14 +13,14 @@ export const Header = () => {
         {/* Main Header */}
         <div className='grid grid-row-2 md:grid-cols-2 gap-8 justify-between align-middle'>
           <div className='grid gap-4'>
-            <div className='flex items-center gap-4'>
+            <div className='flex items-center align-middle gap-4'>
               {/*eslint-disable-next-line @next/next/no-img-element */}
-              <img width='60px' height='60px' src={ProjectLogo.src} alt={`${projectTitle} logo`} />
+              <img className='self-center' width='60px' height='60px' src={ProjectLogo.src} alt={`${projectTitle} logo`} />
               <h1 className='text-5xl font-bold text-secondary-light'>{projectTitle}</h1>
             </div>
           </div>
 
-          <div className='place-self-start md:place-self-end self-center flex gap-2 px-4' >
+          <div className='place-self-start md:place-self-end self-center flex gap-2 px-4 py-4' >
             <a className='place-self-start md:place-self-end self-center flex gap-2 px-4 py-2 font-medium border-2 border-gray-900 text-gray-900' href='https://join.slack.com/t/aquaristlabs/shared_invite/zt-nphn0jhg-QYKw__It8JPMkUR_sArOug' target='_blank' rel="noreferrer">
               <SlackSVG className='fill-current text-gray-900 w-6' />
               Join Slack</a>
